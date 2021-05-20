5cb8b4ac2c700e20f41a5fe468527043b0090c93
Denise Onukwue <donukwue1@gmail.com>
2021-05-20 18:01:08 -0400

    add and conntect React frontend

---
+++ coding-challenge-juice/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const database = require('./config/database');
+
+require("dotenv").config();
+
+require("./config/database");
+const app = express();
+
+// Connect Database
+
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ coding-challenge-juice/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+import react from "react"
+
+export default function App(){
+  return (
+    <h1>Hello World! Welcome to my Recommendation!</h1>
+  );
+   
+}
